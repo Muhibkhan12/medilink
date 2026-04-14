c980c2a2b26e142286bfac79f26e575841e61c3a
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-04-14 20:42:12 +0500

    API working correctly

--- medilink/app/Models/User.php
+++ medilink/app/Models/User.php
@@ -7,9 +7,10 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Tymon\JWTAuth\Contracts\Providers\JWT;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
